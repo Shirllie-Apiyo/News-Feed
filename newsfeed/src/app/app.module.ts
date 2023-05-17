8adad8c604d5248863ee52375191f9f3671f2c70
Shirllie-Apiyo <apraise320@gmail.com>
2023-05-18 01:57:50 +0300

    generate components

--- newsfeed/src/app/app.module.ts
+++ newsfeed/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FriendListComponent } from './friend-list/friend-list.component';
+import { NewsfeedComponent } from './newsfeed/newsfeed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FriendListComponent,
+    NewsfeedComponent
   ],
   imports: [
     BrowserModule,
